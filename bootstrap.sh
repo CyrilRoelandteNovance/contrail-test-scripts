0a004380d1ab388dcb8285acf4ec781914000c67
Sylvain Afchain <sylvain.afchain@enovance.com>
2015-02-03 16:30:15 +0000

    First alltogther version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-	echo "Usage: $0 <dev4x name>"
-	exit -1
-fi
-export DEV4X=$1
-
 if [ -e 'localrc' ]
 then
 	. localrc
 fi
 
+WORK_DIR=${WORK_DIR:-/tmp/}
+WORK_DIR=$WORK_DIR/oc-ci
+
+sudo rm -rf $WORK_DIR
+mkdir $WORK_DIR
+
 export http_proxy=http://pxy.int0.aub.cloudwatt.net:8123
 export https_proxy=http://pxy.int0.aub.cloudwatt.net:8123
 
-if [ "$RECLONE" ]
-then
-	rm -rf contrail-fabric-utils
-	git clone https://github.com/Juniper/contrail-fabric-utils.git
-fi
+# install the last version of fabric
+git clone https://github.com/fabric/fabric.git $WORK_DIR/fabric
+pushd $WORK_DIR/fabric
+sudo python setup.py install
+popd
 
-if [ "$RECLONE" ]
-then
-	rm -rf contrail-test
-	git clone https://github.com/Juniper/contrail-test.git
-fi
+# clone the OC CI test
+git clone https://github.com/Juniper/contrail-test.git $WORK_DIR/contrail-test
+pushd $WORK_DIR/contrail-test
 
+# have to be removed when merged
+git fetch https://review.opencontrail.org/Juniper/contrail-test refs/changes/85/6685/1 && git cherry-pick FETCH_HEAD
+popd
+
+export http_proxy=
+export https_proxy=
+
+# collect informations about the infra
 . /etc/openstack_credentials
 
+# generate the params file
+. params
 
-envsubst < testbed.py.tmpl > testbed.py
+# generate the testbed file
+. testbed
+
+# start the tests
+pushd $WORK_DIR/contrail-test
+
+testr init
+OS_ENDPOINT_TYPE=internalURL PYTHONPATH=fixtures testr run
+
+popd
